64ab9fc8cee52c64ccb40c3f732a7df997e75bb7
jonathan VALERE <jonathan.valere@gmail.com>
2020-11-12 11:31:18 -0300

    user cocktail done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts " Create 3 ingredients"
+puts "Create 3 ingredients"
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
