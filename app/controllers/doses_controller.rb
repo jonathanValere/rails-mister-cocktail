b804e8bb113f0042a9f9f391fac7eb1863c26756
jonathan VALERE <jonathan.valere@gmail.com>
2020-11-13 13:42:04 -0300

    trying to fix cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -19,7 +19,7 @@ class DosesController < ApplicationController
     def destroy
         @dose = Dose.find(params[:id])
         @dose.destroy
-        redirect_to cocktail(@dose.cocktail)
+        redirect_to cocktail_path(@dose.cocktail)
     end
 
     private
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,15 +21,15 @@ Rails.application.configure do
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
-  else
+  }
+else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
-  end
+end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
